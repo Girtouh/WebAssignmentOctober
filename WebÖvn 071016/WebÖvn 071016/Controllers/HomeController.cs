ad27983df5a12a932fe3f10bdda2b2fa467a7e39
Linus Skagerberg <erako97@gmail.com>
2016-10-07 14:24:50 +0200

    Added Experience  Model and created list with said experiences.

--- WebÖvn 071016/WebÖvn 071016/Controllers/HomeController.cs
+++ WebÖvn 071016/WebÖvn 071016/Controllers/HomeController.cs
@@ -3,11 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebÖvn_071016.Models;
 
 namespace WebÖvn_071016.Controllers
 {
     public class HomeController : Controller
     {
+        //Fejkar koppling till en DB för Experience
+
+
+        List<Experience> MyExperienceList;
+
+        public HomeController()
+        {
+            MyExperienceList= List<Experience>():
+            MyExperienceList.Add(new Experience() { Title = "Målare", Description = "Morfars hus", Period = "2016" });
+            MyExperienceList.Add(new Experience() { Title = "Tryckare", Description = "Tryckeri", Period = "2015" });
+            MyExperienceList.Add(new Experience() { Title = "Uteliggare", Description = "Slussen huligan", Period = "1971" });
+
+        }
+
+        //======================================
+
         // GET: Home
         public ActionResult Index()
         {
