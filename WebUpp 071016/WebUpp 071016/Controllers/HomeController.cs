8fc141b026a07ee5a85ea7bc3cb17b46826b09b1
Linus Skagerberg <erako97@gmail.com>
2016-10-07 15:58:22 +0200

    Added style to view, button fuctions added, check "contact" button.

--- WebUpp 071016/WebUpp 071016/Controllers/HomeController.cs
+++ WebUpp 071016/WebUpp 071016/Controllers/HomeController.cs
@@ -13,5 +13,15 @@ namespace WebUpp_071016.Controllers
         {
             return View();
         }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
     }
 }
